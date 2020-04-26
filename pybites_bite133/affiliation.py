701f32c6cab34d0270eece16fb9a88d5c69d8435
pavanrao <pavanraomr@gmail.com>
2020-04-26 10:02:31 -0400

    solved bite 133

--- pybites_bite133/affiliation.py
+++ pybites_bite133/affiliation.py
@@ -1,2 +1,7 @@
 def generate_affiliation_link(url):
-    pass
+    ''' Returns pybytes affiliation link from an Amazon linu
+    '''
+    base_url = 'http://www.amazon.com/dp/'
+    tag = '/?tag=pyb0f-20'
+    id = url.split('/')[5]
+    return F'{base_url}{id}{tag}'
