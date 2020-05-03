fe747a59c1e5ad286f3ed7fd804c43bc4d9f232b
pavanrao <pavanraomr@gmail.com>
2020-05-03 00:22:24 -0400

    Fixed exception path

--- pybites_bite241/test_numbers_to_dec.py
+++ pybites_bite241/test_numbers_to_dec.py
@@ -18,33 +18,30 @@ def test_success():
     assert list_to_decimal([1,2,3,4,5,6,7,8,9,0]) == 1234567890
     
 
-def test_ValueError():
+def test_negative():
     with pytest.raises(ValueError):
         list_to_decimal([-3, 12])
-        list_to_decimal([])
         
 
-def test_TypeError():
+def test_empty():
+    with pytest.raises(ValueError):
+        list_to_decimal([])
+        
+def test_bool():
     with pytest.raises(TypeError):
-        list_to_decimal([6, 2, True])
-        list_to_decimal([3.6, 4, 1])
-        list_to_decimal(['4', 5, 3, 1])
         list_to_decimal([False, True, 0, -1])
+
+def test_str():
+    with pytest.raises(TypeError):
+        list_to_decimal(['4', 5, 3, 1])
+
 
 def test_range():
     with pytest.raises(ValueError):
         list_to_decimal([11,99])
-        list_to_decimal([10,0])
-        
-@pytest.mark.xfail
-def test_mutaion():
-    """
-    This did not helo with testing mutation. 
-    """
-    if(list_to_decimal([10]) == 10):
-        pytest.xfail("Extra test case to pass mutation test")
 
-def test_mutaion_2():
+
+def test_endpoint():
     """
     This test was added so that a mutation would fail the code.
     Pybites uses mut.py to test the tests.
@@ -52,5 +49,13 @@ def test_mutaion_2():
     https://pybit.es/guest-mutpy-exploration.html
     """
     with pytest.raises(ValueError):
-        assert list_to_decimal([10]) == 10
-    
+        list_to_decimal([10,0])
+
+@pytest.mark.xfail
+def test_mutaion():
+    """
+    This did not help with testing mutation. 
+    """
+    if(list_to_decimal([10]) == 10):
+        pytest.xfail("Extra test case to pass mutation test")
+
