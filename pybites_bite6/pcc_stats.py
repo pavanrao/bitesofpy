ac43d849901e2d5a68cffbc8c925b68e5d47629b
pavanrao <pavanraomr@gmail.com>
2020-05-10 16:17:27 -0400

    bite 6 solved

--- pybites_bite6/pcc_stats.py
+++ pybites_bite6/pcc_stats.py
@@ -4,6 +4,8 @@ from collections import Counter, namedtuple
 import os
 import urllib.request
 
+from itertools import tee
+
 # prep
 tmp = os.getenv("TMP", "/tmp")
 tempfile = os.path.join(tmp, 'dirnames')
@@ -14,14 +16,12 @@ urllib.request.urlretrieve(
 
 IGNORE = 'static templates data pybites bbelderbos hobojoe1848'.split()
 
-users, popular_challenges = Counter(), Counter()
+# users, popular_challenges = Counter(), Counter()
 
 Stats = namedtuple('Stats', 'user challenge')
 
 
-# code
-
-def gen_files():
+def gen_files(f):
     """Return a generator of dir names reading in tempfile
 
        tempfile has this format:
@@ -35,14 +35,29 @@ def gen_files():
 
        -> use last column to filter out directories (= True)
     """
-    pass
+    return (line.strip('\n').split(',')[0] 
+            for line in f 
+            if line.strip('\n').split(',')[1] == 'True' 
+            )
 
 
 def diehard_pybites():
     """Return a Stats namedtuple (defined above) that contains the user that
-       made the most PRs (ignoring the users in IGNORE) and a challenge tuple
-       of most popular challenge and the amount of PRs for that challenge.
-       Calling this function on the dataset (held tempfile) should return:
-       Stats(user='clamytoe', challenge=('01', 7))
+        made the most PRs (ignoring the users in IGNORE) and a challenge tuple
+        of most popular challenge and the amount of PRs for that challenge.
+        Calling this function on the dataset (held tempfile) should return:
+        Stats(user='clamytoe', challenge=('01', 7))
     """
-    pass
+    f = open(tempfile, 'r')
+    # Send a file handler to gen_files to get a generator of directories
+    dirs = gen_files(f)
+
+    # Use itertools.tee to split dir iterable into 2 iterators
+    dir1, dir2 = tee(dirs, 2)
+
+    #Now we can Count on 2 copies of iterators
+    users = Counter((line.split('/')[1] for line in dir1 if line.split('/')[1] not in IGNORE))
+    bites = Counter((line.split('/')[0] for line in dir2))
+    f.close()
+    return Stats(users.most_common()[0][0], bites.most_common()[0])
+
