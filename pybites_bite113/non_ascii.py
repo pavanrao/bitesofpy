585a4843ea20ae2e1875f985cf9e9932a51d2922
pavanrao <pavanraomr@gmail.com>
2020-05-20 14:11:48 -0400

    completed bite 113 - refactor

--- pybites_bite113/non_ascii.py
+++ pybites_bite113/non_ascii.py
@@ -1,9 +1,4 @@
 
 def extract_non_ascii_words(text):
     """Filter a text returning a list of non-ascii words"""
-    non_ascii = set()
-    for word in text.split():
-        for letter in word:
-            if ord(letter) < 40 or ord(letter) > 127:
-                non_ascii.add(word)
-    return list(sorted(non_ascii))
+    return [word for word in text.split() if not word.isascii()]
