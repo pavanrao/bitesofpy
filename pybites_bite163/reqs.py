2340acdc3890e802eaf600ee90428e8e74aefcaa
pavanrao <pavanraomr@gmail.com>
2020-05-19 20:50:56 -0400

    completed bite 163 dict without using packaging.version

--- pybites_bite163/reqs.py
+++ pybites_bite163/reqs.py
@@ -1,4 +1,5 @@
-from packaging import version
+# from packaging import version
+# packaging is not installed in pybites
 
 def changed_dependencies(old_reqs: str, new_reqs: str) -> list:
     """Compare old vs new requirement multiline strings
@@ -9,5 +10,9 @@ def changed_dependencies(old_reqs: str, new_reqs: str) -> list:
     new = {req.split('==')[0]: req.split('==')[1] for req in new_reqs.strip().split('\n')}
     return [
        key for key in old 
-       if version.parse(old[key]) < version.parse(new[key])
+       if split_v(old[key]) < split_v(new[key])
     ]
+
+
+def split_v(v: list) -> list:
+    return list(map(int, v.split('.')))
