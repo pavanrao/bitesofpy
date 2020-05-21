3cdb4d7c4bd5eaefbe6226ea1b2df4fed82ef8bf
pavanrao <pavanraomr@gmail.com>
2020-05-21 18:17:45 -0400

    completed bite 225  refactor use sr.translate str.maketrans

--- pybites_bite225/convert_chars.py
+++ pybites_bite225/convert_chars.py
@@ -6,3 +6,9 @@ def convert_pybites_chars(text):
        Return the resulting string."""
     def scase(char): return char if char not in 'PYBITESpybites' else char.swapcase()
     return ''.join([scase(char) for char in text])
+
+
+def convert_pybites_chars_translate(text):
+    """ this one uses str.translate and str.maketrans  """
+    scase = str.maketrans('pybitesPYBITES', 'PYBITESpybites')
+    return text.translate(scase)
