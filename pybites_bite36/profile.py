198988e8499c0c693463f198deb4d83143084bc1
pavanrao <pavanraomr@gmail.com>
2020-05-24 20:51:30 -0400

    completed bite 36 - positional arguments,  *args, **kwargs - refactored

--- pybites_bite36/profile.py
+++ pybites_bite36/profile.py
@@ -1,14 +1,14 @@
-def get_profile(name, age, *args, **kwargs):
+def get_profile(name, age, *sports, **awards):
     if not name:
         raise TypeError('Error: missing required argument: name')
     if not isinstance(age, int):
         raise ValueError('Error: required argument age should be an integer')
-    if len(args) > 5:
+    if len(sports) > 5:
         raise ValueError('Error: get_profile accepts 5 or less optional arguments')
     ret = dict()
     ret['name'] = name
     ret['age'] = age
-    if args: ret['sports'] = sorted(list(args))
-    if kwargs: 
-        ret['awards'] = {key:value for key, value in kwargs.items()}
+    if sports: ret['sports'] = sorted(list(sports))
+    if awards: 
+        ret['awards'] = awards
     return ret
