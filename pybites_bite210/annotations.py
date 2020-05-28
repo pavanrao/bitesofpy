79b4ee72af8f19f9e0437470acfaa5a5b6778a3b
pavanrao <pavanraomr@gmail.com>
2020-05-27 20:51:31 -0400

    completed bite 210 type annotation using typing

---
+++ pybites_bite210/annotations.py
@@ -0,0 +1,13 @@
+from typing import List
+int_list = List[int]
+
+def sum_numbers(numbers :int_list) -> int:
+    """Sums numbers
+
+    :param numbers: a list of numbers
+    :type numbers: list
+    :raises TypeError: if not all numeric values passed in
+    :return: sum of numbers
+    :rtype: int
+    """
+    pass
---
+++ pybites_bite210/test_annotations.py
@@ -0,0 +1,12 @@
+# tests are hidden for this Bite
+# got this after completing the bite
+
+import typing
+
+from annotations import sum_numbers
+
+
+def test_sum_numbers():
+    res = sum_numbers.__annotations__
+    assert res.get('numbers') == typing.List[int]
+    assert res.get('return') == int
