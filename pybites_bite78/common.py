4421e87a08f8bc95c608d6136566faf9674c52f3
pavanrao <pavanraomr@gmail.com>
2020-06-02 19:28:35 -0400

    completed bite 78

---
+++ pybites_bite78/common.py
@@ -0,0 +1,7 @@
+def common_languages(programmers):
+    """Receive a dict of keys -> names and values -> a sequence of
+       of programming languages, return the common languages"""
+    # convert each dict values to set and create a list of sets
+    # unpack the list and pass to set.intersection
+    return set.intersection(* [set(val) for key, val in programmers.items()])
+        
